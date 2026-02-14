fa14e7c9f38c52a8d043c1a5f9730b9ab5cfc2d6
mk-samoilov <me@mk-samoilov.ru>
2026-02-14 16:28:59 +0300

    feat: updated example with action logger & cloc

--- examples/demo/main.go
+++ examples/demo/main.go
@@ -2,33 +2,66 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"g0ui"
 )
 
 func main() {
-	counter := 0
 	w := g0ui.Widgets
 
+	counter := 0
+	var clickLog []string
+
 	g0ui.Run(func() {
-		g0ui.Begin("My program dashboard")
+		g0ui.Begin("g0ui Demo App")
 
 		w.Spacing()
-
-		w.Text("Hello from g0ui example")
-		w.Text(fmt.Sprintf("Counter: %d", counter))
-
+		w.Text("Welcome to g0ui demo application!")
+		w.Text(fmt.Sprintf("Current time: %s", time.Now().Format("15:04:05")))
 		w.Spacing()
 
-		if w.Button("Increment") {
+		w.Separation(30)
+		w.Spacing()
+
+		w.Text(fmt.Sprintf("Counter value: %d", counter))
+		w.Spacing()
+
+		if w.Button("+ Increment") {
 			counter++
+			clickLog = append(clickLog,
+				fmt.Sprintf("[%s] Incremented to %d", time.Now().Format("15:04:05"), counter))
 		}
-		if w.Button("Decrement") {
+		if w.Button("- Decrement") {
 			counter--
+			clickLog = append(clickLog,
+				fmt.Sprintf("[%s] Decremented to %d", time.Now().Format("15:04:05"), counter))
+		}
+		if w.Button("Reset") {
+			counter = 0
+			clickLog = append(clickLog,
+				fmt.Sprintf("[%s] Reset to 0", time.Now().Format("15:04:05")))
 		}
 
 		w.Spacing()
-		w.Separation()
+		w.Separation(32)
+		w.Spacing()
+
+		w.Text("Action log:")
+		if len(clickLog) == 0 {
+			w.Text("  (no actions yet)")
+		} else {
+			start := 0
+			if len(clickLog) > 5 {
+				start = len(clickLog) - 5
+			}
+			for _, entry := range clickLog[start:] {
+				w.Text("  " + entry)
+			}
+		}
+
+		w.Spacing()
+		w.Separation(32)
 		w.Spacing()
 
 		if w.Button("Quit") {
